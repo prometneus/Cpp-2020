1d0f499f8345013880f1eda988bce7c838ab9a82
Artem Burtsev <86663451+prometneus@users.noreply.github.com>
2021-07-25 00:07:25 +0300

    Update main.cpp

--- Pattern Observer/main.cpp
+++ Pattern Observer/main.cpp
@@ -2,34 +2,34 @@
 #include "Observer.h"
 
 int main() {
-	Citizens Oleg, Dima; // Объекты, за которыми следят
+	Citizens Oleg, Dima; // РћР±СЉРµРєС‚С‹, Р·Р° РєРѕС‚РѕСЂС‹РјРё СЃР»РµРґСЏС‚
 	NameCheckist n_one, n_two;
 	StatusCheckist s_one, s_two;
 	NumberCheckist num_one, num_two;
 	std::cout << "NameCheckist 1 has an adress " << &n_one << std::endl;
 	std::cout << "StatusCheckist 1 has an adress " << &s_one << std::endl;
 	std::cout << "NumberCheckist 1 has an adress " << &num_one << std::endl;
-	n_one.subscribe(Oleg); // Подписка наблюдателя на объект
+	n_one.subscribe(Oleg); // РџРѕРґРїРёСЃРєР° РЅР°Р±Р»СЋРґР°С‚РµР»СЏ РЅР° РѕР±СЉРµРєС‚
 	s_one.subscribe(Oleg);
 	num_one.subscribe(Oleg);
-	Oleg.set("Oleg", "Suspicious", 6); // Установка первоначальных данных
-	Oleg.set("Oleg", "Under investigation", 8); // Обновление (установка новых данных)
+	Oleg.set("Oleg", "Suspicious", 6); // РЈСЃС‚Р°РЅРѕРІРєР° РїРµСЂРІРѕРЅР°С‡Р°Р»СЊРЅС‹С… РґР°РЅРЅС‹С…
+	Oleg.set("Oleg", "Under investigation", 8); // РћР±РЅРѕРІР»РµРЅРёРµ (СѓСЃС‚Р°РЅРѕРІРєР° РЅРѕРІС‹С… РґР°РЅРЅС‹С…)
 	Dima.set("Dmitry", "A man of conscience", 0);
-	n_one.subscribe(Dima); // Наблюдатель подписывается на второй объект
+	n_one.subscribe(Dima); // РќР°Р±Р»СЋРґР°С‚РµР»СЊ РїРѕРґРїРёСЃС‹РІР°РµС‚СЃСЏ РЅР° РІС‚РѕСЂРѕР№ РѕР±СЉРµРєС‚
 	s_one.subscribe(Dima);
 	num_one.subscribe(Dima);
-	n_one.unsubscribe(Oleg); // И отписывается от первого
+	n_one.unsubscribe(Oleg); // Р РѕС‚РїРёСЃС‹РІР°РµС‚СЃСЏ РѕС‚ РїРµСЂРІРѕРіРѕ
 	s_one.unsubscribe(Oleg);
 	num_one.unsubscribe(Oleg);
 	std::cout << "NameCheckist 2 has an adress " << &n_two << std::endl;
 	std::cout << "StatusCheckist 2 has an adress " << &s_two << std::endl;
-	std::cout << "NumberCheckist 2 has an adress " << &num_two << std::endl; //Второй наблюдатель
+	std::cout << "NumberCheckist 2 has an adress " << &num_two << std::endl; // Р’С‚РѕСЂРѕР№ РЅР°Р±Р»СЋРґР°С‚РµР»СЊ
 	n_two.subscribe(Dima);
 	s_two.subscribe(Dima);
 	num_two.subscribe(Dima);
 	n_two.subscribe(Oleg);
 	s_two.subscribe(Oleg);
-	num_two.subscribe(Oleg); // Подписывается на оба объекта
+	num_two.subscribe(Oleg); // РџРѕРґРїРёСЃС‹РІР°РµС‚СЃСЏ РЅР° РѕР±Р° РѕР±СЉРµРєС‚Р°
 	Dima.changes();
 	n_one.unsubscribe(Dima);
 	s_one.unsubscribe(Dima);
